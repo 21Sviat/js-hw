4e9572130256dc9c7540592ef1be4e77ebc38eb2
21Sviat <chomko.svt@gmail.com>
2023-08-17 23:42:36 +0300

    hw 1

---
+++ main.js
@@ -0,0 +1,34 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let j = 3.14;
+let k = 2.7;
+let l = 16;
+let m = true;
+let n = false;
+console.log(a,b,c,d,e,f,g,h,j,k,l,m,n);
+
+let firstName = 'Sviatoslav';
+let middleName = 'Bogdanovych';
+let lastName= 'Chomko';
+let mix = `${lastName} ${firstName} ${middleName}`;
+console.log(mix);
+
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+let firstName1 = prompt('як тебе звати?');
+console.log(firstName1)
+let middleName1 = prompt('як ти по-батькові?');
+console.log(middleName1)
+let age = prompt('Скільки тобі років?');
+console.log(age)
